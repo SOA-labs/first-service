097009591e2ddc189236ab32d11889af259f8d7d
Fer0ty <artemiy.soloviev@gmail.com>
2024-11-07 21:58:07 +0300

    first service done

---
+++ ejb-module/src/main/java/ru/artemiyandstepan/model/Color.java
@@ -0,0 +1,7 @@
+package ru.artemiyandstepan.model;
+
+import java.io.Serializable;
+
+public enum Color implements Serializable {
+    GREEN, RED, BLACK, WHITE, YELLOW, ORANGE, BROWN
+}
---
+++ ejb-module/src/main/java/ru/artemiyandstepan/model/Country.java
@@ -0,0 +1,7 @@
+package ru.artemiyandstepan.model;
+
+import java.io.Serializable;
+
+public enum Country implements Serializable {
+    GERMANY, VATICAN, ITALY, SOUTH_KOREA, JAPAN
+}
